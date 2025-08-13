2f4fd6089a56a2f2abc3f05015dcf7c920131f61
luciusvie2712 <vinh15062703@gmail.com>
2025-08-13 13:22:00 +0700

    custom axios

--- src/utils/axios.customize.js
+++ src/utils/axios.customize.js
@@ -1,30 +1,90 @@
 import axios from 'axios'
 
-const instanceAxios = axios.create({
+const instance = axios.create({
   baseURL: 'http://localhost:8888/'
 });
-instanceAxios.defaults.headers.common['Authorization'] = `Bearer`
+
 
 // Add a request interceptor
-instanceAxios.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
+instance.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
     return config;
-  }, function (error) {
-    // Do something with request error
+}, function (error) {
     return Promise.reject(error);
-  });
+});
 
 // Add a response interceptor
-instanceAxios.interceptors.response.use(function (response) {
-
-    // if (response && response.data) return response.data // chi lay data 
-    return response;
-    
-  }, function (error) {
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response && response.data ? response.data : response;
+}, async function as(error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
-  });
+    if (error.status === 401) {
+        const refreshToken = localStorage.getItem('refreshToken')
+        if (refreshToken) {
+            try {
+                const res = await axios.post('/refresh-token', {}, {
+                    headers: {
+                        'Authorization': `Bearer ${refreshToken}`,
+                    }
+                });
+                localStorage.setItem("token", res.accessToken);
+                error.config.headers['Authorization'] = `Bearer ${res.accessToken}`;
+                return axios(error.config);
+            } catch (e) {
+                console.log(e)
+                toast.error("Token expired. Please login again.");
+                window.location.href = '/login';
+            }
+        } else {
+            toast.error("No refresh token available. Please login again.");
+            window.location.href = '/login';
+        }
+    }
 
-export default instanceAxios
+    let errorMessage = ''
+    switch (error.status) {
+        case 400:
+            errorMessage = 'Bad Request: The server could not understand the request.';
+            toast.error(errorMessage)
+
+            break;
+        case 401:
+            errorMessage = 'Unauthorized: Authentication is required.';
+            toast.error(errorMessage)
+            window.location.href = '/login'
+            break;
+        case 403:
+            errorMessage = 'Forbidden: You do not have permission to access this resource.';
+            toast.error(errorMessage)
+            window.location.href = '/404-page'
+            break;
+        case 404:
+            errorMessage = 'Not Found: The requested resource could not be found.';
+            toast.error(errorMessage)
+            break;
+        case 500:
+            errorMessage = 'Internal Server Error: There is a problem with the server.';
+            toast.error(errorMessage)
+            break;
+        case 502:
+            errorMessage = 'Bad Gateway: The server received an invalid response from an upstream server.';
+            toast.error(errorMessage)
+            break;
+        case 503:
+errorMessage = 'Service Unavailable: The server is currently unable to handle the request.';
+            toast.error(errorMessage)
+            break;
+        default:
+            errorMessage = 'An unexpected error occurred.';
+            toast.error(errorMessage)
+            break;
+    }
+
+});
+export default instance
