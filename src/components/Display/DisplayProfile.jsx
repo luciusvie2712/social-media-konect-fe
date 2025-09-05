d9fa9f3fb89a335c80f57d702bc407d90cdc0ed5
luciusvie2712 <vinh15062703@gmail.com>
2025-09-06 01:19:22 +0700

    UI background DisplayProfile with colorthief for getColor of User bgImage

--- src/components/Display/DisplayProfile.jsx
+++ src/components/Display/DisplayProfile.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import { getAUserByIdAPI } from "../../utils/api.customize"
 import { toast } from "react-toastify"
 import avatar from "../../assets/download.png"
 import "../../assets/styles/Profile.scss"
+import bgImage from "../../assets/image/bg_image_profile_2.jpg"
+import ColorThief from "colorthief"
 
 const DisplayProfile = () => {
     const [ userData, setUserData ] = useState(null)
     const user = useSelector((state) => state.user.account)
+    const [bgColor, setBgColor] = useState([0, 0, 0]);
 
     useEffect(() => {
         const fetchAUser = async () => {
@@ -26,11 +29,28 @@ const DisplayProfile = () => {
         }
         fetchAUser()
     }, [user?.id])
+
     return (
-        <div className="display-profile__content">
+        <div className="display-profile__content" style={{background: `linear-gradient(to bottom, rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, 0.8),rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, 0.3), rgba(0,0,0,0.9))`}}>
             {userData ? (
                 <>
-                    <div className="display-profile__header-profile">
+                    <div className="display-profile__header-profile" >
+                        <img
+                            src={bgImage}
+                            alt="cover"
+                            crossOrigin="anonymous"
+                            style={{ opacity: 0, position: "absolute", zIndex: -9999}}
+                            onLoad={(e) => {
+                                try {
+                                const colorThief = new ColorThief();
+                                const [r, g, b] = colorThief.getColor(e.target);
+                                setBgColor([r, g, b]);
+                                console.log("Màu lấy được:", r, g, b);
+                                } catch (err) {
+                                console.error("Không lấy được màu:", err);
+                                }
+                            }}
+                        />
                         <div className="header-profile__avatar">
                             <img src={userData?.avatar || avatar} className="rounded-full w-[150px] border-4 cursor-pointer"  />
                         </div>
@@ -64,10 +84,7 @@ const DisplayProfile = () => {
                             <h1>CHƯA CÓ BÀI VIẾT NÀO</h1>
                         </div>
                     </div>
-                    
                 </>
-
-
             ) : (
                 <p>Loading ...</p>
             )}
