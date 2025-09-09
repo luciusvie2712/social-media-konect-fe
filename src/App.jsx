d4f38260bb48a37bee982e17b2a56969d8aaef9b
Quoc viet <tikiki1011@gmail.com>
2025-09-09 21:58:20 +0700

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,30 @@ import HomePage from "./views/user/HomePage";
 import ForgotPage from "./views/auth/ForgotPage";
 import ResetPassword from "./views/auth/ResetPassword";
 import SideBar from "./components/SideBar";
-
+import GoogleCallback from "./components/Auth/GoogleCallback";
 
 function App() {
-  return ( 
-      <Routes>
-        {/* Auth layout */}
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/forgot-password" element={<ForgotPage />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-
-        {/* User layout */}
-        <Route 
-          path="/*" 
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          } 
-        />
-      </Routes>
-  )
+  return (
+    <Routes>
+      {/* Auth layout */}
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/forgot-password" element={<ForgotPage />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      <Route
+        path="/auth/google/callback/:session_id"
+        element={<GoogleCallback />}
+      />
+      {/* User layout */}
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
