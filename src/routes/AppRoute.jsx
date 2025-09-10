e4df78bfeda795bdf0c0bab21549d229523097e8
Quoc viet <tikiki1011@gmail.com>
2025-09-10 16:59:10 +0700

    hehehe

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,41 +1,40 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-import PrivitaRoute from "../utils/PrivateRoute"
-import AuthLayout from "../components/layout/AuthLayout"
-import AuthPage from "../views/auth/AuthPage"
-import ForgotPage from "../views/auth/ForgotPage"
-import ResetPassword from "../views/auth/ResetPassword"
-import DisplayLayout from "../components/layout/DisplayLayout"
-import HomePage from "../views/user/HomePage"
-import ProfilePage from "../views/user/ProfilePage"
-import GoogleCallback from "../components/Auth/GoogleCallback"
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import PrivitaRoute from "../utils/PrivateRoute";
+import AuthLayout from "../components/layout/AuthLayout";
+import AuthPage from "../views/auth/AuthPage";
+import ForgotPage from "../views/auth/ForgotPage";
+import ResetPassword from "../views/auth/ResetPassword";
+import DisplayLayout from "../components/layout/DisplayLayout";
+import HomePage from "../views/user/HomePage";
+import ProfilePage from "../views/user/ProfilePage";
+import GoogleCallback from "../components/Auth/GoogleCallback";
 
 const AppRoute = () => {
-    return (
-        <Routes>
-            {/* Auth routes */}
-            <Route element={<AuthLayout />}>
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/forgot-password" element={<ForgotPage />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/auth/google/callback/:session_id" element={<GoogleCallback />} />
-            </Route>
+  return (
+    <Routes>
+      {/* Auth routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/forgot-password" element={<ForgotPage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/auth/google/callback" element={<GoogleCallback />} />
+      </Route>
 
-            {/* User routes */}
-            <Route
-                path="/*" 
-                element={
-                    <PrivitaRoute>
-                        <DisplayLayout />
-                    </PrivitaRoute>
-                }
-            >
-                <Route index element={<Navigate to="home" />} />
-                <Route path="home" element={<HomePage />} />
-                <Route path="profile/:id" element={<ProfilePage />} />
-            </Route>
-        </Routes>
-    )
-}
+      {/* User routes */}
+      <Route
+        path="/*"
+        element={
+          <PrivitaRoute>
+            <DisplayLayout />
+          </PrivitaRoute>
+        }
+      >
+        <Route index element={<Navigate to="home" />} />
+        <Route path="home" element={<HomePage />} />
+        <Route path="profile/:id" element={<ProfilePage />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default AppRoute
+export default AppRoute;
