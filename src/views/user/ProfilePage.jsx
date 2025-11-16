4871414fad052b6c9bf1a446f170a38658b919ce
luciusvie2712 <vinh15062703@gmail.com>
2025-11-16 18:54:57 +0700

    feat(ui): create Profile Page

--- src/views/user/ProfilePage.jsx
+++ src/views/user/ProfilePage.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { getAUserByIdAPI } from "../../utils/api.customize";
 import { toast } from "react-toastify";
 import avatar from "../../assets/download.png";
-import "../../styles/Profile.scss";
+import "../../styles/ProfilePage.scss";
 import { useParams } from "react-router-dom";
+import bgDemo from "../../assets/image/bg_image_profile_2.jpg"
 
 const ProfilePage = () => {
   const [ userData, setUserData ] = useState([])
@@ -29,8 +30,66 @@ const ProfilePage = () => {
   }, [id]);
 
   return (
-    <div className="w-full h-screen flex items-center text-black">
-      <span>{userData?.name}</span>
+    <div className="container">
+      {/* Header Profile */}
+      <div className="header-profile">
+        <div className="header-profile__background">
+          <div className="background">
+            {userData?.background ? (
+              <img src={userData.backgroud} alt="background-profile" />
+            ) : (
+              <img src={bgDemo} alt="" />
+            )}
+          </div>
+        </div>
+        <div className="header-profile__user">
+          <div className="user__detail">
+            <div className="avatar-user">
+              <img src={userData?.avatar || avatar} />
+            </div>
+            <div className="fullname-user">
+              <span>{userData?.name}</span>
+            </div>
+          </div>
+          <div className="user__edit">
+            <button className="btn-edit">
+              <i className="fa-solid fa-pen w-4"></i>
+              <span>Chỉnh sửa thông tin</span>
+            </button>
+          </div>
+        </div>
+        <div className="header-profile__stats">
+          <div className="stats-item">
+            <span>1,204</span>
+            <span>Bài viết</span>
+          </div>
+           <div className="stats-item">
+            <span>156</span>
+            <span>Bạn bè</span>
+          </div>
+           <div className="stats-item">
+            <span>5,236</span>
+            <span>Người theo dõi</span>
+          </div>
+        </div>
+
+        <div className="header-profile__active-nav">
+          <div className="active-option">
+            Bài viết
+          </div>
+          <div className="active-option">
+            Giới thiệu
+          </div>
+          <div className="active-option">
+            Bạn bè
+          </div>
+        </div>
+      </div>
+
+      {/* Body Profile */}
+      <div className="body-profile">
+
+      </div>
     </div>
   );
 };
